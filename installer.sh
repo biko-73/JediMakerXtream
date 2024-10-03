dbae1300cd23eb43bfa484cec0d073d25f551363
biko-73 <57875905+biko-73@users.noreply.github.com>
2024-10-03 15:10:33 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,8 +11,8 @@
 ########################################################################################################################
 
 PACKAGE_DIR='JediMakerXtream/main'
-MY_IPK="enigma2-plugin-extensions-jedimakerxtream_6.34_all.ipk"
-MY_DEB="enigma2-plugin-extensions-jedimakerxtream_6.34_all.deb"
+MY_IPK="enigma2-plugin-extensions-jedimakerxtream_6.37_all.ipk"
+MY_DEB="enigma2-plugin-extensions-jedimakerxtream_6.37_all.deb"
 
 
 ########################################################################################################################
